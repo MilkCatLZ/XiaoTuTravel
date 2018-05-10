f591316240801c8d10d7c5d66b494b73781f6687
LZ <v39565306@qq.com>
2018-05-10 18:17:46 +0800

    验证码

--- app/src/main/java/shy/car/sdk/travel/login/ui/VerifyDialogFragment.kt
+++ app/src/main/java/shy/car/sdk/travel/login/ui/VerifyDialogFragment.kt
@@ -1,17 +1,21 @@
 package shy.car.sdk.travel.login.ui
 
 import android.app.Dialog
+import android.content.DialogInterface
 import android.databinding.DataBindingUtil
 import android.databinding.ObservableInt
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
+import com.base.util.Log
 import com.base.util.StringUtils
 import com.base.util.ToastManager
 import io.reactivex.Observable
@@ -31,13 +35,56 @@ import java.util.concurrent.TimeUnit
 class VerifyDialogFragment : XTBaseDialogFragment() {
 
     var currentVerifyNum = ObservableInt(0)
-    var d: Disposable? = null
+
+    private var repeatCount: Int = 0
+    private var d: Disposable? = null
+
+
     lateinit var presenter: VerifyPresenter
+
     @Autowired(name = "phone")
     @JvmField
     var phone: String = ""
 
-    var listener = object : LoginListener {
+    lateinit var binding: FragmentVerifyBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ARouter.getInstance()
+                .inject(this)
+        activity?.let { presenter = VerifyPresenter(listener, it) }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_verify, null, false)
+        binding.fragment = this
+        setData()
+        startCountDown()
+        return binding.root
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        var dialog = super.onCreateDialog(savedInstanceState)
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.setOnKeyListener { _: DialogInterface, _: Int, keyEvent: KeyEvent ->
+            if( keyEvent.action == KeyEvent.ACTION_UP) {
+                if (keyEvent.keyCode == KeyEvent.KEYCODE_DEL && repeatCount == 1) {
+                    backEvent()
+                    repeatCount = 0
+                    true
+                } else {
+                    repeatCount++
+                }
+            }
+
+            false
+        }
+        return dialog
+    }
+
+    /**
+     * 网络回调监听
+     */
+    private var listener = object : LoginListener {
         override fun loginSuccess() {
             dismiss()
         }
@@ -59,69 +106,96 @@ class VerifyDialogFragment : XTBaseDialogFragment() {
         }
     }
 
+    private var currentIsEmpty: Boolean = true
+    /**
+     * 输入监听
+     */
     var textWatcher = object : TextWatcher {
         override fun afterTextChanged(s: Editable) {
             var t = s.toString()
             if (StringUtils.isNotEmpty(t)) {
-                inputEvent()
-            } else {
-                deleteEvent()
+                nextEditText()
             }
         }
 
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            Log.d("beforeTextChanged", s?.toString())
         }
 
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            Log.d("onTextChanged", "onTextChanged")
         }
     }
 
-    lateinit var binding: FragmentVerifyBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        ARouter.getInstance()
-                .inject(this)
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_verify, null, false)
-        binding.fragment = this
-        setData()
-        startCountDown()
-        return binding.root
-    }
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        var dialog = super.onCreateDialog(savedInstanceState)
-        dialog.setCanceledOnTouchOutside(false)
-        return dialog
-    }
-
-    private fun deleteEvent() {
-        when (currentVerifyNum.get()) {
-            0, 1 -> binding.edtVerify0.requestFocus()
-            2 -> binding.edtVerify1.requestFocus()
-            3 -> binding.edtVerify2.requestFocus()
-        }
-    }
-
-    private fun inputEvent() {
-        when (currentVerifyNum.get()) {
-            0 -> {
+    /**
+     * 退回前一个EditText
+     */
+    private fun forwardEditTextRequestFocus(): EditText {
+        return when (currentVerifyNum.get()) {
+            1 -> {
+                currentVerifyNum.set(0)
+                binding.edtVerify0.requestFocus()
+                binding.edtVerify0
+            }
+            2 -> {
                 currentVerifyNum.set(1)
                 binding.edtVerify1.requestFocus()
+                binding.edtVerify1
+            }
+            3 -> {
+                currentVerifyNum.set(2)
+                binding.edtVerify2.requestFocus()
+                binding.edtVerify2
+            }
+            else -> binding.edtVerify0
+        }
+    }
+
+    /**
+     * 下一个editText获取焦点
+     */
+    private fun nextEditText(): EditText {
+        return when (currentVerifyNum.get()) {
+            0 -> {
+                binding.edtVerify1.requestFocus()
+                currentVerifyNum.set(1)
+                binding.edtVerify1
             }
 
             1 -> {
-                currentVerifyNum.set(2)
                 binding.edtVerify2.requestFocus()
+                currentVerifyNum.set(2)
+                binding.edtVerify2
             }
             2 -> {
-                currentVerifyNum.set(3)
                 binding.edtVerify3.requestFocus()
+                currentVerifyNum.set(3)
+                binding.edtVerify3
             }
-            3 -> lockAndSubmit()
+            3 -> {
+                lockAndSubmit()
+                binding.edtVerify3
+            }
+            else -> binding.edtVerify0
         }
+    }
+
+    private fun currentEditText(): EditText {
+        return when (currentVerifyNum.get()) {
+            0 -> binding.edtVerify0
+            1 -> binding.edtVerify1
+            2 -> binding.edtVerify2
+            3 -> binding.edtVerify3
+            else -> binding.edtVerify0
+        }
+    }
+
+    /**
+     * 当
+     */
+    private fun backEvent() {
+        //当输入框已经为空，再次按下退格键，删除前一个editText的内容
+        forwardEditTextRequestFocus().setText("")
     }
 
     fun touchEvent() {
@@ -140,7 +214,9 @@ class VerifyDialogFragment : XTBaseDialogFragment() {
 
     }
 
-
+    /**
+     * 倒计时
+     */
     private fun startCountDown() {
         Observable.intervalRange(1, 60, 0, 1, TimeUnit.SECONDS)
                 .subscribeOn(AndroidSchedulers.mainThread())
@@ -172,6 +248,7 @@ class VerifyDialogFragment : XTBaseDialogFragment() {
         binding.edtVerify1.addTextChangedListener(textWatcher)
         binding.edtVerify2.addTextChangedListener(textWatcher)
         binding.edtVerify3.addTextChangedListener(textWatcher)
+
     }
 
     override fun dismiss() {
