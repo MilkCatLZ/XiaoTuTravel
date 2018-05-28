a7e1350b0a235865cd14ad9fafc768819d7dc055
LZ <v39565306@qq.com>
2018-05-28 14:11:11 +0800

    提交

---
+++ app/src/main/java/shy/car/sdk/travel/location/ui/LocationSelectFragment.kt
@@ -0,0 +1,11 @@
+package shy.car.sdk.travel.location.ui
+
+import shy.car.sdk.app.base.XTBaseFragment
+
+/**
+ * create by LZ at 2018/05/28
+ * 选择地址
+ */
+class LocationSelectFragment:XTBaseFragment() {
+
+}
---
+++ app/src/main/java/shy/car/sdk/travel/location/ui/SelectLocationActivity.kt
@@ -0,0 +1,18 @@
+package shy.car.sdk.travel.location.ui
+
+import android.os.Bundle
+import shy.car.sdk.R
+import shy.car.sdk.app.base.XTBaseActivity
+import shy.car.sdk.app.base.XTBaseFragment
+
+/**
+ * create by LZ at 2018/05/28
+ * 选择地址
+ */
+class LocationSelectActivity:XTBaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_select)
+    }
+}
