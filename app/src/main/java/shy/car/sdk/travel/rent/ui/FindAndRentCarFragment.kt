8963de508dc625e3ad65d4b01cbc37aa41aa62ee
LiuZhi <39565306@qq.com>
2018-06-06 23:14:31 +0800

    地图 路线规划

--- app/src/main/java/shy/car/sdk/travel/rent/ui/FindAndRentCarFragment.kt
+++ app/src/main/java/shy/car/sdk/travel/rent/ui/FindAndRentCarFragment.kt
@@ -6,9 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.amap.api.maps.CameraUpdateFactory
-import com.amap.api.maps.model.BitmapDescriptorFactory
-import com.amap.api.maps.model.LatLng
-import com.amap.api.maps.model.MarkerOptions
+import com.amap.api.maps.model.MyLocationStyle
+import com.amap.api.services.route.*
 import shy.car.sdk.R
 import shy.car.sdk.app.base.XTBaseFragment
 import shy.car.sdk.databinding.FragmentFindAndRentCarBinding
@@ -17,7 +16,7 @@ import shy.car.sdk.databinding.FragmentFindAndRentCarBinding
  * create by lz at 2018/06/05
  * 找车取车
  */
-class FindAndRentCarFragment : XTBaseFragment() {
+class FindAndRentCarFragment : XTBaseFragment(), RouteSearch.OnRouteSearchListener {
 
 
     lateinit var binding: FragmentFindAndRentCarBinding
@@ -33,12 +32,51 @@ class FindAndRentCarFragment : XTBaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.mapView.map.animateCamera(CameraUpdateFactory.zoomTo(10f), 1000, null)
-        binding.mapView.map.moveCamera(CameraUpdateFactory.changeLatLng(LatLng(app.location.lat, app.location.lng)))
-        binding.mapView.map.addMarker(MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_defaul_locat))
-                .anchor(0.5f, 1.0f)
-                .snippet(app.location.address)
-                .position(LatLng(app.location.lat, app.location.lng))
-                .draggable(false))
+//        binding.mapView.map.moveCamera(CameraUpdateFactory.changeLatLng(LatLng(app.location.lat, app.location.lng)))
+        initMap()
+//        binding.mapView.map.addMarker(MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_defaul_locat))
+//                .anchor(0.5f, 1.0f)
+//                .snippet(app.location.address)
+//                .position(LatLng(app.location.lat, app.location.lng))
+//                .draggable(false))
+        getRoute()
+    }
+
+    private fun getRoute() {
+        val routeSearch = RouteSearch(activity)
+
+        val query = RouteSearch.DriveRouteQuery(RouteSearch.FromAndTo(), RouteSearch.DRIVING_SINGLE_SHORTEST, null, null, "")
+
+        routeSearch.setRouteSearchListener(object : RouteSearch.OnRouteSearchListener {
+            override fun onDriveRouteSearched(p0: DriveRouteResult?, p1: Int) {
+
+            }
+
+            override fun onBusRouteSearched(p0: BusRouteResult?, p1: Int) {
+
+            }
+
+            override fun onRideRouteSearched(p0: RideRouteResult?, p1: Int) {
+
+            }
+
+            override fun onWalkRouteSearched(p0: WalkRouteResult?, p1: Int) {
+
+            }
+
+        })
+
+        routeSearch.calculateDriveRoute(query)
+    }
+
+    private fun initMap() {
+        val myLocationStyle = MyLocationStyle()
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW)
+        //初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.interval(2000) //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        binding.mapView.map.myLocationStyle = myLocationStyle//设置定位蓝点的Style
+//aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
+        binding.mapView.map.isMyLocationEnabled = true
     }
 
     override fun onResume() {
