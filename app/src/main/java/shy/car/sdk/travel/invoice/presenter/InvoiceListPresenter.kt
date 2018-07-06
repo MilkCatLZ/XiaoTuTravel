219ed3bbae76951367bca417a5837dbb6fc2b0f2
LZ <v39565306@qq.com>
2018-07-06 09:18:39 +0800

    提交

--- app/src/main/java/shy/car/sdk/travel/invoice/presenter/InvoiceListPresenter.kt
+++ app/src/main/java/shy/car/sdk/travel/invoice/presenter/InvoiceListPresenter.kt
@@ -5,6 +5,7 @@ import android.databinding.ObservableArrayList
 import android.support.v7.widget.RecyclerView
 import android.widget.CheckBox
 import com.base.databinding.DataBindingItemClickAdapter
+import com.base.databinding.ItemViewHolder
 import com.base.widget.FullLinearLayoutManager
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
@@ -18,34 +19,48 @@ import shy.car.sdk.travel.invoice.data.InvoiceList
 class InvoiceListPresenter(context: Context, var callBack: CommonCallBack<List<InvoiceList>>) : BasePresenter(context) {
 
     val checkList = ObservableArrayList<InvoiceList.Orders>()
-    val invoiceList = ArrayList<InvoiceList>()
+    private val invoiceList = ArrayList<InvoiceList>()
     val adapter = DataBindingItemClickAdapter<InvoiceList>(R.layout.item_invoice_list, BR.invoice, BR.click, {
 
     }, { holder, position ->
+        setupInvoice(holder, position)
+    })
+
+    private fun setupInvoice(holder: ItemViewHolder<*>, position: Int) {
 
         val orderAdapter = DataBindingItemClickAdapter<InvoiceList.Orders>(R.layout.item_invoice_list_detail, BR.orders, BR.click, {}, { holder, position ->
-            val checkBox = holder.binding.root.findViewById<CheckBox>(R.id.check_order)
-            checkBox.setOnCheckedChangeListener { p0, p1 ->
-                if (p1) {
-                    checkList.add(checkBox.tag as InvoiceList.Orders)
-                } else {
-                    checkList.remove(checkBox.tag)
-                }
-            }
-            holder.binding.setVariable(BR.presenter,this@InvoiceListPresenter)
+            setupOrders(holder, position)
         })
-        orderAdapter.setItems(invoiceList[position].orders,1)
+        orderAdapter.setItems(invoiceList[position].orders, 1)
         val recycler = holder.binding.root.findViewById<RecyclerView>(R.id.recyclerView_invoice_order)
         recycler.layoutManager = FullLinearLayoutManager(context)
         recycler.adapter = orderAdapter
-
-    })
-
-    init {
-
     }
 
-    fun getInvoiceList() {
+    private fun setupOrders(holder: ItemViewHolder<*>, position: Int) {
+        val checkBox = holder.binding.root.findViewById<CheckBox>(R.id.check_order)
+        checkBox.setOnCheckedChangeListener { p0, p1 ->
+            if (p1) {
+                checkList.add(checkBox.tag as InvoiceList.Orders)
+            } else {
+                checkList.remove(checkBox.tag)
+            }
+        }
+        holder.binding.setVariable(BR.presenter, this@InvoiceListPresenter)
+    }
+
+    init {
+        val list = ArrayList<InvoiceList>()
+        for (i in 1..10) {
+            val invoice = InvoiceList()
+            invoice.id = i
+            list.add(invoice)
+        }
+
+        adapter.setItems(list, 1)
+    }
+
+    private fun getInvoiceList() {
         val observable = ApiManager.getInstance()
                 .api.getInvoiceList(offset = (pageIndex - 1) * pageSize, limit = pageSize)
         val observer = object : Observer<List<InvoiceList>> {
