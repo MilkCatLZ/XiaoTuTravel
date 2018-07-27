ba46cacceff958a405c8ce4da3cca59f60ed76f7
LZ <v39565306@qq.com>
2018-07-27 09:14:53 +0800

    提交

--- app/src/main/java/shy/car/sdk/travel/rent/ui/CarRentFragment.kt
+++ app/src/main/java/shy/car/sdk/travel/rent/ui/CarRentFragment.kt
@@ -446,7 +446,7 @@ class CarRentFragment : XTBaseFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_car_rent, null, false)
         binding.map.onCreate(savedInstanceState)
-        binding.content.startRippleAnimation()
+        binding.diffuseView.start()
         setBinding()
         return binding.root
     }
@@ -759,7 +759,7 @@ class CarRentFragment : XTBaseFragment() {
 
     override fun onDestroy() {
         //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
-        binding.content.stopRippleAnimation()
+        binding.diffuseView.stop()
         binding.map.onDestroy()
         nearCarListener = null
         super.onDestroy()
